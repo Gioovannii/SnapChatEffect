6b150cad1a8e3f8c030a1dfa8deecedb2182524e
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-25 20:43:16 +0800

    remove imageName parameter

--- SnapChatEffect/SnapChatEffectApp.swift
+++ SnapChatEffect/SnapChatEffectApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SnapChatEffectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(imageName: "haloween-1")
+            ContentView()
         }
     }
 }
